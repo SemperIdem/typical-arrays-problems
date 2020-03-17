50a627435cbfa42dd14e79e305f9a05b2e16c536
SemperIdem <faintler915@gmail.com>
2020-03-18 00:29:57 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0) {
+    return array.reduce((min, current) => {
+      return min >= current ? current : min;
+    })
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0) {
+    return array.reduce((max, current) => {
+      return max >= current ? max : current;
+    })
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+    return (array.reduce((avg, current) => {
+      return avg + current
+    }))/ array.length
+  }
   return 0;
 }
